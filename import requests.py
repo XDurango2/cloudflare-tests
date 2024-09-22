8ac067da20bf909517357f850c967a7872b17745
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-09-22 15:11:48 -0700

    Initial commit

---
+++ import requests.py
@@ -0,0 +1,19 @@
+import requests
+
+
+API_BASE_URL = "https://api.cloudflare.com/client/v4/accounts/a71faaf431a55b28fbf7c2bfbe9c1fba/ai/run/"
+headers = {"Authorization": "Bearer U2z-hQPH0ZY80xKsPgFpuQ_haaTksaai10MzU8jh"}
+
+
+def run(model, inputs):
+    input = { "messages": inputs }
+    response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
+    return response.json()
+
+
+inputs = [
+    { "role": "system", "content": "You are a friendly assistan that helps write stories" },
+    { "role": "user", "content": "Write a short story about a llama that goes on a journey to find an orange cloud "}
+];
+output = run("@cf/meta/llama-3-8b-instruct", inputs)
+print(output)
